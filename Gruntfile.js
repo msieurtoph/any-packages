f6f049eec8d73120ce2d24add50b331e6b3de0f8
msieurtoph <chr.pot@gmail.com>
2014-12-22 04:14:35 +0100

    Connect codeclimate to Travis CI

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,7 @@ module.exports = function (grunt) {
 
   // Define the configuration for all the tasks
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
     // Make sure code styles are up to par and there are no obvious mistakes
     jshint: {
@@ -43,7 +44,16 @@ module.exports = function (grunt) {
     },
 
     'jasmine_node': {
-      all: ['spec/']
+      all: ['spec/'],
+      coverage: {
+        options : {
+          failTask: false,
+          branches : 100 ,
+          functions: 100,
+          statements:100,
+          lines:100
+        }
+      }
     }
 
   });
@@ -51,7 +61,7 @@ module.exports = function (grunt) {
   grunt.registerTask('default', [
     'jshint:all',
     'test',
-    'build'
+    //'build',
   ]);
 
   grunt.registerTask('test', [
